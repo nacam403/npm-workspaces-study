096549da239d832b929aec15418a3fc896c20afa
nacam403 <satnakam@gmail.com>
2021-12-30 15:39:01 +0900

    ドットファイルを整理。

--- packages/AwesomeTSProject/.eslintrc.js
+++ packages/AwesomeTSProject/.eslintrc.js
@@ -1,10 +1,7 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: "@react-native-community",
   rules: {
-    'no-shadow': 'off',
-    '@typescript-eslint/no-shadow': ['error'],
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": ["error"],
   },
 };
