d93b60bc8acd937bf23e0c2a1e2c6c941d4ebb6a
nacam403 <satnakam@gmail.com>
2021-12-30 17:15:15 +0900

    Lint関係の整理。

--- packages/AwesomeTSProject/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: "@react-native-community",
-  rules: {
-    "no-shadow": "off",
-    "@typescript-eslint/no-shadow": ["error"],
-  },
-};
--- packages/AwesomeTSProject/metro.config.js
+++ packages/AwesomeTSProject/metro.config.js
@@ -5,6 +5,7 @@
  * @format
  */
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require("path");
 
 const watchFolders = [path.resolve(__dirname, "../../node_modules")];
